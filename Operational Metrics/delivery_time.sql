103f4f6ff1b3ef087ef3082cad6ba57419caee98
asad-foodpanda <77100717+asad-foodpanda@users.noreply.github.com>
2021-07-08 19:23:32 +0500

    update delivery time

--- Operational Metrics/delivery_time.sql
+++ Operational Metrics/delivery_time.sql
@@ -5,18 +5,21 @@ DECLARE end_utc date DEFAULT '2021-04-01';
 
 DECLARE chains ARRAY <STRING> DEFAULT ['cm4kh', 'cz5ud', 'cz8ck', 'co5oz'];
 
-select avg(o.delivery_time_in_minutes),
-from `fulfillment-dwh-production.pandata_curated.pd_orders` o
-join `fulfillment-dwh-production.pandata_curated.pd_vendors` v
-  on o.vendor_code = v.vendor_code and o.global_entity_id = v.global_entity_id 
-join `fulfillment-dwh-production.pandata_curated.sf_accounts` a
-  on a.global_entity_id = v.global_entity_id and a.global_vendor_id = v.global_vendor_id 
+Select
+Avg (o.actual_delivery_time_in_seconds/60) as Delivery_time,
 
-where a.is_marked_for_testing_training = false
-  and v.is_test = false
-  and v.is_private = false
-  and v.chain_code in unnest(chains)
-  and o.created_date_local between start_date and end_date
-  and o.created_date_utc between start_utc and end_utc
-  and o.is_test_order = false
-  and o.is_valid_order 
+from
+(`fulfillment-dwh-production.pandata_curated.lg_orders` o
+      left join unnest(deliveries) as c on c.is_primary and DATE(c.rider_dropped_off_at_local) between Date_Sub(date_trunc(Current_Date(),isoweek),Interval 1 YEAR) and date_sub(current_date(), interval 1 day) 
+  AND DATE_DIFF(DATE(c.rider_dropped_off_at_local), DATE(o.order_placed_at_local), DAY) <= 1
+      )
+join `fulfillment-dwh-production.pandata_curated.pd_vendors` v
+on v.vendor_code = left(o.code,4)
+and v.global_entity_id = o.global_entity_id
+
+where
+o.created_date_utc >= start_utc
+and o.global_entity_id = 'FP_PK'
+and o.order_status = 'completed'
+and o.created_date_local between start_date and end_date
+and v.chain_code in unnest(chains)
